df78d71bc8c8c717ff9b9e8d17b7274cf389f134
Mayank Kushal <mayankkushal26@gmail.com>
2021-07-26 20:36:34 +0530

    fix Platform issue on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:window_size/window_size.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    setWindowFrame(Rect.fromLTWH(100.0, 200.0, 1300.0, 700.0));
-  }
+  // TODO: find handeling for this, which is supported in web
+  // try {
+  //   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+  //     setWindowFrame(Rect.fromLTWH(100.0, 200.0, 1300.0, 700.0));
+  //   }
+  // } on Exception catch (e) {
+  //   print("Skipping frame size on web");
+  // }
   await GetStorage.init();
   runApp(App());
 }
